e965a76ef8c112a6275d47d8f5fd6a0747db392d
OlekZima <zimenkoalex34@gmail.com>
2024-04-19 23:20:05 +0200

    Constructor and ToString() reactor

--- MP02/Coach.cs
+++ MP02/Coach.cs
@@ -5,37 +5,26 @@ namespace MP02;
 public class Coach
 {
     private readonly List<Archer> _archers = new();
-    public List<string> Names { get; init; }
+
+    public Coach(List<string> names)
+    {
+        Names = names;
+    }
+
+    private List<string> Names { get; init; }
 
     public void AddArcher(Archer archer)
     {
-        if (_archers.Contains(archer))
-        {
-            return;
-        }
+        if (_archers.Contains(archer)) return;
         _archers.Add(archer);
         archer.AddCoach(this);
     }
 
     public override string ToString()
     {
-        var stringBuilder = new StringBuilder("Coach ");
-        foreach (var name in Names) stringBuilder.Append($"{name} ");
+        var coachNames = string.Join(" ", Names);
+        var archerNames = _archers.Select(a => string.Join(" ", a.Names));
 
-        var archersNames = new StringBuilder("is training archers: ");
-        foreach (var archer in _archers)
-        {
-            var archerNamesStr = new StringBuilder();
-            var whiteChar = " ";
-            for (var i = 0; i < archer.Names.Count; i++)
-            {
-                if (i == archer.Names.Count) whiteChar = "; ";
-                archerNamesStr.Append($"{archer.Names[i]}{whiteChar}");
-            }
-
-            archersNames.Append(archerNamesStr);
-        }
-
-        return stringBuilder.Append(archersNames).ToString();
+        return $"Coach {coachNames} is training archers: {string.Join("; ", archerNames)}";
     }
 }
