2e473511cfeb20c3fb751fa1c4488942b1b57d05
OlekZima <zimenkoalex34@gmail.com>
2024-04-19 23:19:34 +0200

    Changed Archer class to represent a Part in a composition from UML

--- MP02/Archer.cs
+++ MP02/Archer.cs
@@ -4,8 +4,25 @@ namespace MP02;
 
 public class Archer
 {
+    private readonly Bow _bow;
     private Coach? _coach;
+
+    private Archer(Bow bow, List<string> names)
+    {
+        _bow = bow;
+        Names = names;
+    }
+
     public List<string> Names { get; init; }
+
+    public static Archer CreateArcher(Bow? bow, List<string> names)
+    {
+        if (bow == null) throw new Exception("This bow does not exist!");
+
+        var archer = new Archer(bow, names);
+        bow.AddArcher(archer);
+        return archer;
+    }
 
 
     public void AddCoach(Coach coach)
@@ -20,7 +37,6 @@ public class Archer
     {
         var namesStr = new StringBuilder("Archer ");
         foreach (var name in Names) namesStr.Append($"{name} ");
-
-        return $"{namesStr}has a coach {_coach}";
+        return $"{namesStr}has a coach {_coach}, {_bow}";
     }
 }
