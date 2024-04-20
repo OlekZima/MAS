bd623e9ebfa151e6e593824c69b1d89d666a42d2
OlekZima <zimenkoalex34@gmail.com>
2024-04-21 00:37:52 +0200

    Add test examples

--- MP02/Program.cs
+++ MP02/Program.cs
@@ -1,24 +1,23 @@
 ﻿using MP02;
 
 // Asocjacja kwalifikowana (całość)
-var bow1 = new Bow(0, Material.Carbon);
+var bow1 = new Bow(Material.Wood);
 
 // Asocjacja kwalifikowana (część)
 // Asocjacja "zwykła"
+// Asoscjacja z atrybutem
 var archer1 = Archer.CreateArcher(bow1, ["Jakiś", "Debil"]);
 
 // Asocjacja "zwykła"
 var coach1 = new Coach(["Roman", "Pękalski"]);
-
-var compteition1 = new Competition("Mistrzostwa Polski");
-var compteition2 = new Competition("Mistrzostwa Polski 2");
-var compteition3 = new Competition("Mistrzostwa Polski 3");
-Console.WriteLine(compteition1.Id);
-Console.WriteLine(compteition2.Id);
-Console.WriteLine(compteition3.Id);
-
-
 archer1.AddCoach(coach1);
+
+// Asoscjacja z atrybutem
+var compteition1 = new Competition("Mistrzostwa Polski");
+var compete1 = new Compete(compteition1, archer1, 600, new DateOnly(2021, 12, 12));
+
 
 Console.WriteLine(archer1);
 Console.WriteLine(coach1);
+
+Console.WriteLine(compete1);
